73f74a368cd7c36b770c6889964b020ad72cade1
Nowan <dniprovskyi.m@gmail.com>
2024-08-30 21:27:31 +0200

    fixed sheep flock buffer & view radius

--- src/scenes/flock/Chicken/Chicken.gd
+++ src/scenes/flock/Chicken/Chicken.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 @export var max_speed: = 200.0
-@export var mouse_follow_force: = -0.05
+@export var mouse_follow_force: = 0.05
 @export var cohesion_force: = 0.05
 @export var algin_force: = 0.05
 @export var separation_force: = 0.05
-@export var view_distance := 150.0
-@export var avoid_distance := 120.0
+@export var view_distance := 50.0
+@export var avoid_distance := 20.0
+
 
 var _width = ProjectSettings.get_setting("display/window/size/viewport_width")
 var _height = ProjectSettings.get_setting("display/window/size/viewport_height")
@@ -15,20 +16,22 @@ var _flock: Array = []
 var _mouse_target: Vector2
 var _velocity: Vector2
 
-var _status: FlockTypes.BoidStatus = FlockTypes.BoidStatus.IDLE
-	
+
 func _ready():
 	randomize()
-	#mouse_follow_force = 0
-	#_velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * max_speed
-	#_mouse_target = get_random_target()
+	_velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * max_speed
+	_mouse_target = get_random_target()
 
-func set_status(status: FlockTypes.BoidStatus):
-	_status = status
-	for f in _flock:
-		var body = f as RigidBody2D
-		var node = body.get_node(body.name)
-		# TODO: check type conversion
+
+func _on_FlockView_body_entered(body: PhysicsBody2D):
+	print("entered", body)
+	if self != body:
+		_flock.append(body)
+
+
+func _on_FlockView_body_exited(body: PhysicsBody2D):
+	print("exited", body)
+	_flock.remove_at(_flock.find(body))
 
 
 func _input(event):
@@ -39,22 +42,12 @@ func _input(event):
 			_mouse_target = get_random_target()
 
 
-func _physics_process(delta):
-	match _status:
-		FlockTypes.BoidStatus.IDLE:
-			_physics_process_idle(delta)
-		FlockTypes.BoidStatus.FLEEING:
-			_physics_process_fleeing(delta)
-
-func _physics_process_idle(delta):
-	var mouse_vector = Vector2.ZERO
-			
-func _physics_process_fleeing(delta: float):
+func _physics_process(_delta):
 	var mouse_vector = Vector2.ZERO
 	if _mouse_target != Vector2.INF:
 		mouse_vector = global_position.direction_to(_mouse_target) * max_speed * mouse_follow_force
 	
-	## get cohesion, alginment, and separation vectors
+	# get cohesion, alginment, and separation vectors
 	var vectors = get_flock_status(_flock)
 	
 	# steer towards vectors
@@ -66,14 +59,10 @@ func _physics_process_fleeing(delta: float):
 	
 	_velocity = (_velocity + acceleration).limit_length(max_speed)
 	
-	look_at(position + _velocity)
-	rotate(-PI * 0.5)
-
-	#DebugDraw2d.line_vector(position, _velocity);
-	
 	set_velocity(_velocity)
 	move_and_slide()
 	_velocity = velocity
+
 
 func get_flock_status(flock: Array):
 	var center_vector: = Vector2()
@@ -96,7 +85,6 @@ func get_flock_status(flock: Array):
 		align_vector /= flock_size
 		flock_center /= flock_size
 
-
 		var center_dir = global_position.direction_to(flock_center)
 		var center_speed = max_speed * (global_position.distance_to(flock_center) / $FlockView/ViewRadius.shape.radius)
 		center_vector = center_dir * center_speed
@@ -107,12 +95,3 @@ func get_flock_status(flock: Array):
 func get_random_target():
 	randomize()
 	return Vector2(randf_range(0, _width), randf_range(0, _height))
-
-
-func _on_flock_view_body_entered(body: Node2D) -> void:
-	if self != body:
-		_flock.append(body)
-
-
-func _on_flock_view_body_exited(body: Node2D) -> void:
-	_flock.remove_at(_flock.find(body))
