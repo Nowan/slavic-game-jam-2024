5dc661405789c776dde11820e417c0cd50ee8b41
Michał Łazowik <mlazowik@gmail.com>
2024-08-30 23:36:42 +0200

    Lobby and dog sync prototype

--- src/scenes/dog/dog.gd
+++ src/scenes/dog/dog.gd
@@ -3,27 +3,52 @@ extends Sprite2D
 var _angular_speed = PI
 var _dog_speed = 500.0
 
+var _direction := 0
+var _velocity := Vector2.ZERO
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if multiplayer.is_server():
+		set_multiplayer_authority(multiplayer.get_unique_id())
+	else:
+		set_multiplayer_authority(multiplayer.get_peers()[0])
+		
+	print("Unique id: ", multiplayer.get_unique_id())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var direction = 0
-	if Input.is_action_pressed("move_left"):
-		direction = -1
-	if Input.is_action_pressed("move_right"):
-		direction = 1
-	
-	rotation += _angular_speed * direction * delta
-	
-	var 	velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed("move_up"):
-		velocity = Vector2.DOWN.rotated(rotation) * _dog_speed
+	if is_multiplayer_authority():
+		print("I have authority!")
+		_direction = 0
+		if Input.is_action_pressed("move_left"):
+			_direction = -1
+		if Input.is_action_pressed("move_right"):
+			_direction = 1
 		
-	if Input.is_action_pressed("move_down"):
-		velocity = Vector2.UP.rotated(rotation) * _dog_speed
+		_velocity = Vector2.ZERO
+		
+		if Input.is_action_pressed("move_up"):
+			_velocity = Vector2.DOWN.rotated(rotation) * _dog_speed
+			
+		if Input.is_action_pressed("move_down"):
+			_velocity = Vector2.UP.rotated(rotation) * _dog_speed
+			
+		set_pos_and_motion.rpc(position, rotation, _direction, _velocity)
+	else:
+		print("I don't!")
 
-	position += velocity * delta
+	rotation += _angular_speed * _direction * delta
+	position += _velocity * delta
+
+@rpc("unreliable")
+func set_pos_and_motion(pos: Vector2, rot: float, direction: int, velocity: Vector2) -> void:
+	# print(pos)
+	# print(rot)
+	# print(direction)
+	# print(velocity)
+	print("set called!")
+	position = pos
+	rotation = rot
+	_direction = direction
+	_velocity = velocity
