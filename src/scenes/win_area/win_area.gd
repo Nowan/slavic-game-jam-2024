b672edf51c6a0c966ba4ef93d9ee410982804934
Patryk Chrabaszcz <patryk.ch@yggdrasilgaming.com>
2024-08-31 20:56:23 +0200

    fix typo

--- src/scenes/win_area/win_area.gd
+++ src/scenes/win_area/win_area.gd
@@ -34,5 +34,5 @@ func _on_body_exited(body: Node2D) -> void:
 	if (body is Dog):
 		currentPlayersInWinArea -= 1
 	if (body.is_in_group("sheep")):
-		currentSheepsInWinArea -= 1gs
+		currentSheepsInWinArea -= 1
 		
