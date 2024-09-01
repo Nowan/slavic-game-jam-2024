dd690f099723c9b9c1703d8a98b2482106bbe597
Patryk Chrabaszcz <patryk.ch@yggdrasilgaming.com>
2024-09-01 12:11:15 +0200

    fixed sheeps death sfx

--- src/scenes/sheep_death_zone/SheepDeathZone.gd
+++ src/scenes/sheep_death_zone/SheepDeathZone.gd
@@ -5,6 +5,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("sheep")):
+		$AudioStreamPlayer.play()
 		WinArea.deadSheeps += 1
 		#(body as Sheep).die_from_fall()
 		body.queue_free()
