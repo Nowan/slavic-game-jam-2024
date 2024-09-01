354d0edbfdfcdaa0844ed3cc2d4977b8f1483ee0
Nowan <dniprovskyi.m@gmail.com>
2024-09-01 06:38:36 +0200

    added death indicator when ship falls out

--- src/scenes/sheep_death_zone/SheepDeathZone.gd
+++ src/scenes/sheep_death_zone/SheepDeathZone.gd
@@ -1,7 +1,20 @@
 extends Area2D
 
+@export var DeathIndicator: PackedScene = null
+@export var ui_indicators_parent: Node2D = null
+
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("sheep")):
 		WinArea.deadSheeps += 1
 		(body as Sheep).die_from_fall()
+		_show_death_indicator_above_body(body)
 		
+func _show_death_indicator_above_body(body: Node2D):
+	var death_indicator = DeathIndicator.instantiate()
+	var position = ui_indicators_parent.to_local(body.global_position)
+	var animation_player = death_indicator.find_child("AnimationPlayer") as AnimationPlayer
+	
+	death_indicator.position = position
+	ui_indicators_parent.add_child(death_indicator)
+	
+	animation_player.play("default")
