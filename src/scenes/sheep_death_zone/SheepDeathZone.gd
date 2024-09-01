eeb2455964852c19f6b8a7600ab16914dbf79b63
Nowan <dniprovskyi.m@gmail.com>
2024-09-01 10:22:59 +0200

    updated sheep asset & remove buggy death animation

--- src/scenes/sheep_death_zone/SheepDeathZone.gd
+++ src/scenes/sheep_death_zone/SheepDeathZone.gd
@@ -6,7 +6,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("sheep")):
 		WinArea.deadSheeps += 1
-		(body as Sheep).die_from_fall()
+		#(body as Sheep).die_from_fall()
+		body.queue_free()
 		_show_death_indicator_above_body(body)
 		
 func _show_death_indicator_above_body(body: Node2D):
