1ed8ebfb38230823aa132cde3a57a6bfd5c94877
Michał Łazowik <mlazowik@gmail.com>
2024-09-01 05:50:40 +0200

    Kill server and client on any disconnect
    So that we can keep the server starting itself again and again:
    the quickest brute force way to provider support for multiple
    playthoughts in the showcase without manually restarting the server all
    the time

--- src/scenes/lobby/lobby.gd
+++ src/scenes/lobby/lobby.gd
@@ -1,18 +1,16 @@
 extends Control
 
+const SERVER_IP = "188.245.152.157"
 # Default game server port. Can be any number between 1024 and 49151.
 # Not present on the list of registered or common ports as of May 2024:
 # https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
 const DEFAULT_PORT = 8910
 const MAX_PLAYERS = 10
 
-@onready var address: LineEdit = $Address
-@onready var host_button: Button = $HostButton
-@onready var join_button: Button = $JoinButton
+@onready var status_text: LineEdit = $Status
+@onready var play_button: Button = $PlayButton
 @onready var status_ok: Label = $StatusOk
 @onready var status_fail: Label = $StatusFail
-@onready var port_forward_label: Label = $PortForward
-@onready var find_public_ip_button: LinkButton = $FindPublicIP
 
 var players = {}
 var player_info = {"name": "Name"}
@@ -77,16 +75,16 @@ func _player_connected(_id: int) -> void:
 func _register_player(new_player_info):
 	var new_player_id = multiplayer.get_remote_sender_id()
 	players[new_player_id] = new_player_info
+	_set_status(str(players.size()) + " players ready", true)
 	# player_connected.emit(new_player_id, new_player_info)
 
 func _player_disconnected(_id: int) -> void:
 	players.erase(_id)
 	
-	if multiplayer.is_server():
-		_end_game("Client disconnected.")
-	else:
+	if _id == 1:
 		_end_game("Server disconnected.")
-
+	
+	_end_game("One of the players disconnected.")
 
 # Callback from SceneTree, only for clients (not server).
 func _connected_ok() -> void:
@@ -102,8 +100,7 @@ func _connected_fail() -> void:
 	_set_status("Couldn't connect.", false)
 
 	multiplayer.set_multiplayer_peer(null)  # Remove peer.
-	host_button.set_disabled(false)
-	join_button.set_disabled(false)
+	play_button.set_disabled(false)
 
 
 func _server_disconnected() -> void:
@@ -112,15 +109,20 @@ func _server_disconnected() -> void:
 
 #region Game creation methods
 func _end_game(with_error: String = "") -> void:
-	if has_node("/root/Pong"):
+	print("end game")
+	
+	# TODO(mlazowik): better end handling
+	get_tree().quit()
+	
+	# print(get_tree().get_root().get_tree_string())
+	if has_node("/root/Main"):
 		# Erase immediately, otherwise network might show
 		# errors (this is why we connected deferred above).
-		get_node(^"/root/Pong").free()
+		get_node(^"/root/Main").free()
 		show()
 
 	multiplayer.set_multiplayer_peer(null)  # Remove peer.
-	host_button.set_disabled(false)
-	join_button.set_disabled(false)
+	play_button.set_disabled(false)
 
 	_set_status(with_error, false)
 
@@ -146,24 +148,13 @@ func _on_host_pressed() -> void:
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 
 	multiplayer.set_multiplayer_peer(peer)
-	host_button.set_disabled(true)
-	join_button.set_disabled(true)
 	_set_status("Waiting for player...", true)
 	get_window().title = ProjectSettings.get_setting("application/config/name") + ": Server"
 
-	# Only show hosting instructions when relevant.
-	port_forward_label.visible = true
-	find_public_ip_button.visible = true
-
 
 func _connect_to_server():
-	var ip := address.get_text()
-	if not ip.is_valid_ip_address():
-		_set_status("IP address is invalid.", false)
-		return
-
 	peer = ENetMultiplayerPeer.new()
-	var error = peer.create_client(ip, DEFAULT_PORT)
+	var error = peer.create_client(SERVER_IP, DEFAULT_PORT)
 	if error:
 		return error
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
@@ -175,6 +166,3 @@ func _connect_to_server():
 
 func _on_join_pressed():
 	load_game()
-
-func _on_find_public_ip_pressed() -> void:
-	OS.shell_open("https://icanhazip.com/")
